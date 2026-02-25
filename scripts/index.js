ff6d9308e20b02ab6d35edf2f56ef39a6e595419
MaduDev003 <mariabevenuto062@gmail.com>
2026-02-25 17:18:03 -0300

    feat: toggle theme

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,19 @@
-function changeTheme() {
+function toggleTheme() {
+    const checkbox = document.getElementById("theme-checkbox");
+    const cart = document.querySelector(".cart-icon");
     const body = document.body;
-    body.classList.toggle('light');
+    if (checkbox.checked) {
+        body.classList.remove("light");
+        body.classList.add("dark");
+
+        cart.classList.remove("light");
+        cart.classList.add("dark");
+    } else {
+        body.classList.remove("dark");
+        body.classList.add("light");
+
+        cart.classList.remove("dark");
+        cart.classList.add("light");
+    
+    }
 }
